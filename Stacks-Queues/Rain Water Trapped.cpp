dba00011461f869e78cb57884f9cb949b75c7d65
Gunjan-Shekhawat <65302818+Gunjan-Shekhawat@users.noreply.github.com>
2020-06-30 22:56:22 +0530

    Update Rain Water Trapped.cpp

--- Stacks-Queues/Rain Water Trapped.cpp
+++ Stacks-Queues/Rain Water Trapped.cpp
@@ -28,3 +28,30 @@ PROBLEM : Given an integer array A of non-negative integers representing an elev
            https://www.interviewbit.com/problems/rain-water-trapped/
 
 */
+
+int Solution::trap(const vector<int> &A) {
+int result = 0;
+int n = A.size();
+int i=0, j=n-1;
+int maxLeft = A[i], maxRight = A[j];
+
+if(n<3)
+    return 0;
+
+while(i<j)    
+{
+    if(A[i]<A[j])
+    {
+        maxLeft = max(maxLeft, A[++i]);
+        result += maxLeft - A[i];
+    }
+    else
+    {
+        maxRight = max(maxRight, A[--j]);
+        result += maxRight - A[j]; 
+    }
+}
+return result;
+}
+
+
