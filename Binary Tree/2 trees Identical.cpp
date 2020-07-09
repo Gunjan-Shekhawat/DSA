3064830c712bc0880f9a3e1b6632648c4b364719
Gunjan-Shekhawat <65302818+Gunjan-Shekhawat@users.noreply.github.com>
2020-07-10 03:19:41 +0530

    Create 2 trees Identical.cpp

---
+++ Binary Tree/2 trees Identical.cpp
@@ -0,0 +1,160 @@
+/*
+Since this is a functional problem you don't have to worry about input, you just have to complete the function isIdentical() that takes two roots as parameters and returns true or false. The printing is done by the driver code.
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 <= T <= 100
+1 <= Number of nodes <= 1000
+1 <=Data of a node <= 1000
+
+Example:
+Input:
+3
+1 2 3
+1 2 3
+1 2 3
+1 3 2
+1 2 3 N 3 N 10
+1 2 3 N 3 N 10
+Output:
+Yes
+No
+Yes
+
+Explanation:
+Testcase 1: There are two trees both having 3 nodes and 2 edges, both trees are identical having the root as 1, left child of 1 is 2 and right child of 1 is 3.
+Testcase 2: There are two trees both having 3 nodes and 2 edges, but both trees are not identical.
+Testcase 3: There are two trees which are totally identical to each other.
+
+*/
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+bool isIdentical(Node* a, Node* b);
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string str, str1;
+        getline(cin, str);
+        Node *rootA = buildTree(str);
+        getline(cin, str1);
+        Node *rootB = buildTree(str1);
+        if (isIdentical(rootA, rootB)) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+/* A binary tree node
+
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+/* Should return true if trees with roots as r1 and 
+   r2 are identical */
+bool isIdentical(Node *r1, Node *r2)
+{
+    if(r1==NULL && r2==NULL)
+        return true;
+    else if(r1==NULL || r2==NULL)
+        return false;
+    if(r1->data == r2->data && isIdentical(r1->left,r2->left) && isIdentical(r1->right,r2->right)) 
+    return true;
+    else 
+    return false;
+}
